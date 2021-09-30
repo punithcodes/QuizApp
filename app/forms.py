febca9ed69fa5bb1096336a32f97723be8f5eded
Puneeth M S <68369945+punithcodes@users.noreply.github.com>
2021-09-30 21:07:22 +0530

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -2,16 +2,18 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
-
+# Here I have imported and inherited Django's inbuilt AuthenticationForm. This class is responsible for user login form.
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
 
+# Here I have imported and inherited Django's inbuilt UserCreationForm for Signup. 
 class SignupForm(UserCreationForm):
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-
+    
+    # I have used Meta class and used Django's inbuilt User model to create form.
     class Meta:
         model = User
         fields = ['username', 'email']
